1f0c5930380a3e3600ff9e0177096d9520167f43
Vinod Kumar <vinodkumarnkt61@gmail.com>
2022-11-03 12:40:47 +0500

    Initial commit

---
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Hello World of Spring
+
+<br><br>
+
+Student name: ${param.studentName}
+
+<br><br>
+
+The Message: ${message}
+</body>
+
+</html>
---
+++ src/com/geeks/springdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.geeks.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
